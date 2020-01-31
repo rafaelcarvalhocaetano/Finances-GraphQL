a5f8f4250c30562a915f16f5af9b069339cbb0ac
Rafael Carvalho <rapha.pse@outlook.com>
2020-01-31 20:58:19 -0300

    Send Port PostGres with docker

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { getUserId } = require('../util');
+const moment = require('moment');
 
 function accounts(_, args, ctx, info) {
   const userID = getUserId(ctx);
@@ -17,6 +18,38 @@ function accounts(_, args, ctx, info) {
     },
     orderBy: 'description_ASC'
   }, info)
+}
+
+function records(_, { month, type, accountsIds, categoriesIds }, ctx, info) {
+  const userId = getUserId(ctx);
+
+  let AND = [ { user: { id: userId }}]
+  AND = !type ? AND : [ ...AND, { type : type }]
+
+  AND = !accountsIds || accountsIds.length === 0 ? AND :
+  [ ...AND, {
+    OR: accountsIds.map(id => ({ account: { id: id }}))
+  }];
+
+  AND = !categoriesIds || categoriesIds.length === 0 ? AND :
+  [ ...AND, {
+    OR: categoriesIds.map(id => ({ category: { id: id }}))
+  }];
+  if (month) {
+    const date = moment(month, 'MM-YYYY');
+    const startDate = date.startOf('month').toISOString();
+    const endDate = date.endOf('month').toISOString();
+    AND = [
+      ...AND,
+      { date_gte: startDate },
+      { date_lte: endDate }
+    ]
+  }
+
+  return ctx.db.query.records({
+    where: { AND },
+    orderBy: 'date_ASC'
+  }, info);
 }
 
 function categories(_, { operation }, ctx, info) {
@@ -51,5 +84,6 @@ function user(_, args, ctx, info) {
 module.exports = {
   accounts,
   categories,
+  records,
   user
 }
